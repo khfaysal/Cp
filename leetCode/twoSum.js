e17915f22ad10e3af8c5f5792a914a2605a08422
Faysal <k.hasanfaysal@gmail.com>
2026-03-01 00:31:37 +0600

    leetCode second problem solved

---
+++ leetCode/twoSum.js
@@ -0,0 +1,15 @@
+function twoSum(array, target) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+  return null;
+}
+
+const arr = [0, 1, 2, 3, 4, 5];
+const result = twoSum(arr, 2);
+console.log(result);
+
